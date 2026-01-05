716b978c6984413d6dd243d56eedf204efb9a546
Kirill Kravchenko <ayrek2015@yandex.ru>
2026-01-05 18:28:39 +0300

    fix: 4 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -74,8 +74,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date);
+  const daysToAdd = (12 - result.getUTCDay()) % 7 || 7;
+  result.setUTCDate(result.getUTCDate() + daysToAdd);
+  return result;
 }
 
 /**
